049209da1a315f78206bbb6081338250b332df37
Berkan Özgür <berkanozgur@hotmail.com>
2022-10-10 20:46:39 +0300

    Carousel fixes.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,22 +3,25 @@ import CarouselImage2 from "../assets/images/carousel2.png"
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
+
+const handleDragStart = (e: any) => e.preventDefault();
+
 const responsive = {
     1024: { items: 1 }
 };
 
 const items = [
     <div className="carousel-items">
-        <img src={CarouselImage} />
+        <img src={CarouselImage} onDragStart={handleDragStart} />
     </div>,
     <div className="carousel-items">
-        <img src={CarouselImage2} />
+        <img src={CarouselImage2} onDragStart={handleDragStart} />
     </div>,
     <div className="carousel-items">
-        <img src={CarouselImage} />
+        <img src={CarouselImage} onDragStart={handleDragStart} />
     </div>,
     <div className="carousel-items">
-        <img src={CarouselImage2} />
+        <img src={CarouselImage2} onDragStart={handleDragStart} />
     </div>,
 ]
 
