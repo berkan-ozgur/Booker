ed64a3d54040e2c00d63c67b0c105cfdc52957ef
Emre Şahin <190254064@ogr.alanya.edu.tr>
2022-10-10 22:19:30 +0300

    BW-11 Card design updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,33 @@
 import bookImage from "../assets/images/bookImage.png";
-import foto from "../assets/images/foto.png";
+// https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918681/heart.png
+
+{
+  /* <button type="button" className="btn btn-primary btn-sm">
+  Add To Cart
+</button> */
+}
 
 const BookCard = () => {
   return (
     <div className="wrapper">
-      <div className="card">
-        <img src={bookImage} className="book-image" alt="" />
-        <div className="card-container">
-          <h1 className="book-title">Book Name</h1>
-          <p className="book-description">Book description</p>
-          <button type="button" className="btn btn-primary">
-            Add To Cart
-          </button>
-          <button type="button" className="btn btn-primary">
-            <i className="pi pi-heart"></i>
+      <div className="products-card">
+        <div className="card">
+          <img src={bookImage} className="book-image" alt="" />
+          <div className="card-container">
+            <h2 className="book-title">Book Name</h2>
+            <p className="book-description">Book description</p>
+            <div className="heartIcon text-center">
+              <i className="pi pi-heart"></i>
+            </div>
+          </div>
+        </div>
+          <button className="btn btn-primary"  type="button">
+            <p className="add-to-cart"> Add To Cart</p>
+            
           </button>
         </div>
       </div>
-    </div>
+   
   );
 };
 export default BookCard;
