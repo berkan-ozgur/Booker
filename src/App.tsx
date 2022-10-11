f3352db4e92b710b0e29df840326a119fa3ceec7
Berkan Özgür <berkanozgur@hotmail.com>
2022-10-11 13:43:42 +0300

    Contact us page done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import './App.scss';
 import "primeicons/primeicons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
 
 
 function AppRoutes() {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/aboutus", element: <AboutUs /> }
+    { path: "/aboutus", element: <AboutUs /> },
+    { path: "/contact", element: <Contact /> }
   ]);
   return routes;
 }
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,105 @@
+import Layout from "../components/Layout"
+
+
+const Contact = () => {
+    return (
+        <Layout>
+            <h4 className="mt-3">İletişim</h4>
+            <hr className="mt-3"></hr>
+            <div className="contact">
+                <div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Firma Adı:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Firma Resmi Adı:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Telefon:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>E-mail:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Adres:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Ülke:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Şehir:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Semt:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Vergi No:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Vergi Dairesi:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr /><div className="row">
+                    <div className="col-lg-4 header">
+                        <p>Havale Bildirim Formu:</p>
+                    </div>
+                    <div className="col-lg-8">
+                        BES
+                    </div>
+                </div>
+                <hr />
+            </div>
+        </Layout>
+    )
+}
+
+export default Contact
